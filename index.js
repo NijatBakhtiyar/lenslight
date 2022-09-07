9bf5d38717cd89b9b859fcd9725beaf4defafa36
NijatBakhtiyar <nijatbakhtiyar@gmail.com>
2022-09-07 10:00:06 +0400

    install template engine

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+// ejs template engine
+app.set("view engine", "ejs");
+
+// static files middleware
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Index Page");
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
